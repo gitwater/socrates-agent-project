aa5dd50f15b3e278027f116a4823f64ef3bd0215
Kevin Lindsay <klindsay@waterbear.cloud>
2024-12-07 16:31:37 -0800

    - Removed redundant code - Made the agent's prompts configurable

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 
-from socratic_agent import Agent
+from socratic_agent import SocraticAgent
 from user import User
 from cli import CLI
 
@@ -21,8 +21,24 @@ class SessionState:
         self.all_questions_to_the_user = ""
         self.cli = CLI(client_id)
         self.user = User(self)
-        #self.agent = Agent(self)
-        self.agent = Agent(self, "gpt-4o")
+        # Aidan Agent Config
+        agent_config = {
+                # Socrates and Theaetetus are two AI assistants for the User to:
+                'purpose': 'that will assist the User in answering their questions, providing feedback, and/or engaging with the user to help them improve their self awareness',
+                'user_input': 'The users input is as follows',
+                # Socrates and Theaetetus will engage in multi-round dialogue to:
+                'engagement_purpose': 'analyze the Users current state and input to provide the best answer, feedback, and/or engagement for the User.',
+                # Their discussion should follow a:
+                'discussion_strategy': """structured problem-solving approach, such as formalizing the problem, developing
+high-level strategies for solving the problem, reusing subproblem solutions where possible, critically evaluating
+each other's reasoning, avoiding logical errors, and effectively communicating their ideas.""",
+                # Their ultimate objective is to:
+                'ultimate_objective': 'come to the next best response through reasoned discussion.',
+                # If they encounter any issues with the validity of their answer, :
+                'if_answer_not_valid': 'they should re-evaluate their reasoning and calculations.'
+            }
+        #self.agent = SocraticAgent(self, agent_config)
+        self.agent = SocraticAgent(self, agent_config, "gpt-4o")
 
 def main():
     session = SessionState(default_client_id)
--- src/socratic_agent.py
+++ src/socratic_agent.py
@@ -6,16 +6,39 @@ import time
 
 debug_printing = False
 
-class SocraticAgent:
+class Agent:
 
-    def __init__(self, persona, model="gpt-3.5-turbo"):
+    def __init__(self, persona, prompts=None, model="gpt-3.5-turbo"):
         self.persona = persona
+        self.other_persona = None
         self.model = model
         self.history = []
         self.gpt_client = openai.OpenAI(
             # This is the default and can be omitted
             api_key=os.getenv("OPENAI_API_KEY")
         )
+        if persona == "Socrates":
+            self.other_persona = "Theaetetus"
+        elif persona == "Theaetetus":
+            self.other_persona = "Socrates"
+
+        if prompts == None:
+            self.prompts = {
+                'purpose': 'solve challenging promblems.',
+                'user_input': 'The users input is as follows',
+                'engagement_purpose': 'solve the problem together for the User.',
+                'discussion_strategy': """structured problem-solving approach , such as formalizing the problem, developing
+    high-level strategies for solving the problem, reusing
+    subproblem solutions where possible, critically evaluating each other's reasoning, avoiding
+    logical errors, and effectively communicating their ideas.""",
+                'ultimate_objective': 'come to a correct solution through reasoned discussion.',
+                'if_answer_not_valid': 'they should re-evaluate their reasoning and calculations.'
+            }
+        else:
+            self.prompts = prompts
+
+    def set_prompts(self, prompts):
+        self.prompts = prompts
 
     def get_gpt_response(self, messages):
         try:
@@ -31,7 +54,7 @@ class SocraticAgent:
         except openai.OpenAIError as e:
             if "maximum context length" in str(e):
                 # Handle the maximum context length error here
-                msg = "The context length exceeds my limit... "
+                msg = "The context length exceeds my limit..."
             else:
                 # Handle other errors here
                 msg = f"I enconter an when using my backend model.\n\n Error: {str(e)}"
@@ -65,50 +88,67 @@ class SocraticAgent:
             "content": f"Message from a proofreader Plato to you two: {proofread}"
         })
 
-class Socrates(SocraticAgent):
-    def __init__(self, model="gpt-3.5-turbo"):
-        super().__init__('Socrates', model)
-        self.other_role = "Theaetetus"
+    def set_question(self, user_input):
+        system_role = f"""
+Socrates and Theaetetus are two AI assistants {self.prompts['purpose']}.
 
-    def set_question(self, question):
+{self.prompts['user_input']}: "{user_input}".
+
+Socrates and Theaetetus will engage in multi-round dialogue to {self.prompts['engagement_purpose']}.
+
+They are permitted to consult with the User if they encounter any uncertainties or difficulties,
+by using the following phrase: "@Check with the User: [insert your question]". Any responses from
+the User will be provided in the following round.
+
+Their discussion should follow a {self.prompts['discussion_strategy']}.
+
+Their ultimate objective is to {self.prompts['ultimate_objective']}.
+
+To present their final answer, they should adhere to the following guidelines:
+
+State the problem they were asked to solve.
+Present any assumptions they made in their reasoning.
+Detail the logical steps they took to arrive at their final answer.
+Conclude with a final statement that directly answers the problem.
+Their final answer should be concise and free from logical errors, such as false dichotomy, hasty generalization, and circular reasoning.
+
+It should begin with the phrase: "Here is our @final answer: [insert answer]".
+
+If they encounter any issues with the validity of their answer, {self.prompts['if_answer_not_valid']}.
+
+Now, suppose that you are {self.persona}. Please discuss the problem with {self.other_persona}!"""
+
+        if self.persona == 'Plato':
+            system_role += "\n\nNow as a proofreader, Plato, your task is to read through the dialogue between Socrates and Theaetetus and identify any errors they made."
+
         self.history.append({
             "role": "system",
-                "content": f"Socrates and Theaetetus are two AI assistants for the User to solve challenging problems. The problem statement is as follows: \"{question}\".\n\nSocrates and Theaetetus will engage in multi-round dialogue to solve the problem together for the User. They are permitted to consult with the User if they encounter any uncertainties or difficulties, by using the following phrase: \"@Check with the User: [insert your question]\". Any responses from the User will be provided in the following round. Their discussion should follow a structured problem-solving approach, such as formalizing the problem, developing high-level strategies for solving the problem, writing Python scripts when necessary, reusing subproblem solutions where possible, critically evaluating each other's reasoning, avoiding arithmetic and logical errors, and effectively communicating their ideas. \n\nThey are encouraged to write and execute Python scripts. To do that, they must follow the following instructions:\n1. use the phrase \"@write_code [insert python scripts wrapped in a markdown code block]\". \n2. use the phrase \"@execute\" to execute the previously written Python scripts. \n\nE.g., \n@write_code\n```\ndef f(n):\n	return n+1\n\n	print(f(n))\n```\n\n@execute\n\nAll these scripts will be sent to a subprocess.Popen() object that runs in the backend. The system will provide the output and error messages from executing their Python scripts in the subsequent round.\n\nTo aid them in their calculations and fact-checking, they are also allowed to consult WolframAlpha. They can do so by using the phrase \"@Check with WolframAlpha: [insert your question]\", and the system will provide responses in the subsequent round.\n\nTheir ultimate objective is to come to a correct solution through reasoned discussion. To present their final answer, they should adhere to the following guidelines:\n\nState the problem they were asked to solve.\nPresent any assumptions they made in their reasoning.\nDetail the logical steps they took to arrive at their final answer.\nVerify any mathematical calculations with WolframAlpha to prevent arithmetic errors.\nConclude with a final statement that directly answers the problem.\nTheir final answer should be concise and free from logical errors, such as false dichotomy, hasty generalization, and circular reasoning. \n\nIt should begin with the phrase: \"Here is our @final answer: [insert answer]\". If they encounter any issues with the validity of their answer, they should re-evaluate their reasoning and calculations.\n\nNow, suppose that you are {self.persona}. Please discuss the problem with {self.other_role}!"}
-        )
+            "content": system_role
+        })
+
+
+        assistant_role_prefix = ""
+        if self.persona == "Plato":
+            assistant_role_prefix = "Socrates: "
+        assistant_role = f"{assistant_role_prefix}: Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any mistakes."
+
         self.history.append({
             "role": "assistant",
-            "content": f"Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any mistakes."
+            "content": assistant_role
         })
 
-class Theaetetus(SocraticAgent):
-    def __init__(self, model="gpt-3.5-turbo"):
-        super().__init__('Theaetetus', model)
+class SocratesAgent(Agent):
+    def __init__(self, prompts=None, model="gpt-3.5-turbo"):
+        super().__init__('Socrates', prompts, model)
 
-        self.other_role = "Socrates"
 
-    def set_question(self, question):
-            self.history.append({
-                "role": "system",
-                 "content": f"Socrates and Theaetetus are two AI assistants for the User to solve challenging problems. The problem statement is as follows: \"{question}\".\n\nSocrates and Theaetetus will engage in multi-round dialogue to solve the problem together for the User. They are permitted to consult with the User if they encounter any uncertainties or difficulties, by using the following phrase: \"@Check with the User: [insert your question]\". Any responses from the User will be provided in the following round. Their discussion should follow a structured problem-solving approach, such as formalizing the problem, developing high-level strategies for solving the problem, writing Python scripts when necessary, reusing subproblem solutions where possible, critically evaluating each other's reasoning, avoiding arithmetic and logical errors, and effectively communicating their ideas. \n\nThey are encouraged to write and execute Python scripts. To do that, they must follow the following instructions:\n1. use the phrase \"@write_code [insert python scripts wrapped in a markdown code block]\". \n2. use the phrase \"@execute\" to execute the previously written Python scripts. \n\nE.g., \n@write_code\n```\ndef f(n):\n	return n+1\n\n	print(f(n))\n```\n\n@execute\n\nAll these scripts will be sent to a subprocess.Popen() object that runs in the backend. The system will provide the output and error messages from executing their Python scripts in the subsequent round.\n\nTo aid them in their calculations and fact-checking, they are also allowed to consult WolframAlpha. They can do so by using the phrase \"@Check with WolframAlpha: [insert your question]\", and the system will provide responses in the subsequent round.\n\nTheir ultimate objective is to come to a correct solution through reasoned discussion. To present their final answer, they should adhere to the following guidelines:\n\nState the problem they were asked to solve.\nPresent any assumptions they made in their reasoning.\nDetail the logical steps they took to arrive at their final answer.\nVerify any mathematical calculations with WolframAlpha to prevent arithmetic errors.\nConclude with a final statement that directly answers the problem.\nTheir final answer should be concise and free from logical errors, such as false dichotomy, hasty generalization, and circular reasoning. \n\nIt should begin with the phrase: \"Here is our @final answer: [insert answer]\". If they encounter any issues with the validity of their answer, they should re-evaluate their reasoning and calculations.\n\nNow, suppose that you are {self.persona}. Please discuss the problem with {self.other_role}!"}
-            )
-            self.history.append({
-                "role": "user",
-                "content": f"Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any mistakes."
-            })
+class TheaetetusAgent(Agent):
+    def __init__(self, prompts=None, model="gpt-3.5-turbo"):
+        super().__init__('Theaetetus', prompts, model)
 
-class Plato(SocraticAgent):
-    def __init__(self, model="gpt-3.5-turbo"):
-        super().__init__('Plato', model)
-
-    def set_question(self, question):
-        self.history.append({
-            "role": "system",
-                "content": f"Socrates and Theaetetus are two AI assistants for the User to solve challenging problems. The problem statement is as follows: \"{question}\".\n\nSocrates and Theaetetus will engage in multi-round dialogue to solve the problem together for the User. They are permitted to consult with the User if they encounter any uncertainties or difficulties, by using the following phrase: \"@Check with the User: [insert your question]\". Any responses from the User will be provided in the following round. Their discussion should follow a structured problem-solving approach, such as formalizing the problem, developing high-level strategies for solving the problem, writing Python scripts when necessary, reusing subproblem solutions where possible, critically evaluating each other's reasoning, avoiding arithmetic and logical errors, and effectively communicating their ideas. \n\nThey are encouraged to write and execute Python scripts. To do that, they must follow the following instructions:\n1. use the phrase \"@write_code [insert python scripts wrapped in a markdown code block]\". \n2. use the phrase \"@execute\" to execute the previously written Python scripts. \n\nE.g., \n@write_code\n```\ndef f(n):\n	return n+1\n\n	print(f(n))\n```\n\n@execute\n\nAll these scripts will be sent to a subprocess.Popen() object that runs in the backend. The system will provide the output and error messages from executing their Python scripts in the subsequent round.\n\nTo aid them in their calculations and fact-checking, they are also allowed to consult WolframAlpha. They can do so by using the phrase \"@Check with WolframAlpha: [insert your question]\", and the system will provide responses in the subsequent round.\n\nTheir ultimate objective is to come to a correct solution through reasoned discussion. To present their final answer, they should adhere to the following guidelines:\n\nState the problem they were asked to solve.\nPresent any assumptions they made in their reasoning.\nDetail the logical steps they took to arrive at their final answer.\nVerify any mathematical calculations with WolframAlpha to prevent arithmetic errors.\nConclude with a final statement that directly answers the problem.\nTheir final answer should be concise and free from logical errors, such as false dichotomy, hasty generalization, and circular reasoning. \n\nIt should begin with the phrase: \"Here is our @final answer: [insert answer]\". If they encounter any issues with the validity of their answer, they should re-evaluate their reasoning and calculations.\n\nNow as a proofreader, Plato, your task is to read through the dialogue between Socrates and Theaetetus and identify any errors they made."}
-        )
-        self.history.append({
-            "role": "user",
-            "content": f"Socrates: Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any mistakes."
-        })
+class PlatoAgent(Agent):
+    def __init__(self, prompts=None, model="gpt-3.5-turbo"):
+        super().__init__('Plato', prompts, model)
 
     def proofread(self):
         pf_template = {
@@ -127,13 +167,13 @@ class Plato(SocraticAgent):
                 })
             return msg
 
-class Agent:
+class SocraticAgent:
 
-    def __init__(self, session, model="gpt-3.5-turbo"):
+    def __init__(self, session, prompts=None, model="gpt-3.5-turbo"):
         self.session = session
-        self.socrates = Socrates(model)
-        self.theaetetus = Theaetetus(model)
-        self.plato = Plato(model)
+        self.socrates = SocratesAgent(prompts, model)
+        self.theaetetus = TheaetetusAgent(prompts, model)
+        self.plato = PlatoAgent(prompts, model)
 
     def need_to_ask_the_User(self, text):
         pattern = r"@Check with the User:\s*(.*)"
@@ -171,7 +211,7 @@ class Agent:
                 self.session.dialog_lead, self.session.dialog_follower = self.socrates, self.theaetetus
                 return json.dumps([
                     {'role':'Socrates',
-                    'response': f"Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any logical or mathematical mistakes.\n"}
+                    'response': f"Hi Theaetetus, let's solve this problem together. Please feel free to correct me if I make any logical mistakes.\n"}
                     ])
             else:
                 if self.session.in_progress_sub == False and self.session.wait_for_the_user == False:
